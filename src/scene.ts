799c6f04efaf43f1a60b81e41465b8564001812e
Alexandru Damian <alexdamian121@gmail.com>
2023-02-24 10:07:20 +0200

    MFG11:Fixed y offset

--- src/scene.ts
+++ src/scene.ts
@@ -1,45 +1,23 @@
 import * as BABYLON from "@babylonjs/core";
 import { GridMaterial } from "@babylonjs/materials/grid/gridMaterial";
 
-function normalizePos(vec3: Array<number>, referencePos) {
-  const SIZE = 3;
-
-  if (vec3.length < SIZE) {
-    return vec3;
-  }
-
-  console.log("BEFORE: "+ vec3);
-  for (let index = 0; index < vec3.length; ++index ) {
-    if (vec3[index] % referencePos != 0) {
-      vec3[index] += referencePos / 2;
-    }
-  }
-
-  console.log("AFTER: "+ vec3);
-
-  return vec3;
-}
-
 export default class Playground {
   private readonly boxSize = 1;
 
   private readonly spaceBoxSize = 30;
-  private readonly groundHeight = (this.boxSize + 0.01) / 2;
-
   private readonly decelarationDeltaY = 32;
 
   private createCube(color: string, pos: BABYLON.Vector3, sizes): BABYLON.Mesh {
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
 
-    //sizes = normalizePos(sizes, this.boxSize);
     box.scaling = new BABYLON.Vector3(sizes[0], sizes[1], sizes[2]);
 
     box.edgesWidth = 1;
     box.edgesColor = new BABYLON.Color4(1, 1, 1, 1);
-    box.setPivotPoint(new BABYLON.Vector3(this.boxSize/2,this.boxSize/2,this.boxSize/2));
+    box.setPivotPoint(new BABYLON.Vector3(this.boxSize/2,0,this.boxSize/2));
     box.position = pos;
 
-    box.position.y = this.groundHeight;
+    box.position.y = box.scaling.y/2;
 
     const boxMat = new BABYLON.StandardMaterial("boxMat");
     boxMat.diffuseColor = BABYLON.Color3.FromHexString(color);
@@ -102,49 +80,49 @@ export default class Playground {
     let cube = this.createCube("#4A6DE5", new BABYLON.Vector3(), [1, 1, 1]);
     let cube1 = this.createCube(
       "#4912E5",
-      new BABYLON.Vector3(3, 1, 4),
+      new BABYLON.Vector3(3, 0, 4),
       [1, 2, 1]
     );
     let cube3 = this.createCube(
       "#43D100",
-      new BABYLON.Vector3(2, 1, 6),
+      new BABYLON.Vector3(2, 0, 6),
       [2, 1, 1]
     );
     let cube4 = this.createCube(
       "#97D1FF",
-      new BABYLON.Vector3(5, 3, 0),
+      new BABYLON.Vector3(5, 0, 0),
       [1, 1, 2]
     );
     let cube5 = this.createCube(
       "#4338DC",
-      new BABYLON.Vector3(6, 3, 7),
-      [1, 2, 2]
+      new BABYLON.Vector3(6, 0, 7),
+      [1, 2, 3]
     );
     let cube7 = this.createCube(
       "#FFD100",
-      new BABYLON.Vector3(-10, -4, 4),
+      new BABYLON.Vector3(-10, 0, 4),
       [2, 1, 2]
     );
     let cube8 = this.createCube(
       "#43FF00",
-      new BABYLON.Vector3(-6, -0, 8),
+      new BABYLON.Vector3(-6, 0, 8),
       [2, 2, 1]
     );
     let cube9 = this.createCube(
       "#57D100",
-      new BABYLON.Vector3(-4, -8, 4),
-      [2, 2, 2]
+      new BABYLON.Vector3(-4, 0, 4),
+      [3, 3, 3]
     );
 
     cube.actionManager = new BABYLON.ActionManager(scene);
 
     let ground = this.createPlane();
-    let currentMesh: BABYLON.Nullable<BABYLON.AbstractMesh>;
+    let currentMesh;
 
     let previousPosition;
 
-    let previousY = this.groundHeight;
-    let currentY = this.groundHeight;
+    let previousY = 0;
+    let currentY = previousY;
 
     let getGroundPosition = () => {
       // Use a predicate to get position on the ground
@@ -186,7 +164,6 @@ export default class Playground {
         if (currentMesh == ground || !currentMesh) {
           return;
         }
-
         currentMesh?.enableEdgesRendering();
         previousPosition = currentMesh.position;
 
@@ -210,7 +187,7 @@ export default class Playground {
             : currentMesh.position.y;
         currentY = currentY - evt.movementY / this.decelarationDeltaY;
         if (Math.abs(currentY - previousY) > 0) {
-          currentMesh.position.y = this.snap(currentY, currentMesh.scaling.y);
+          currentMesh.position.y = this.snap(currentY, currentMesh.scaling.y) + currentMesh.scaling.y/2;
         }
 
         return;
