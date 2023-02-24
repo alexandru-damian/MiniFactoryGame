c3a12b2526880b9ce597222ba24003fb5e84748a
Alexandru Damian <alexdamian121@gmail.com>
2023-02-24 09:34:56 +0200

    MFG11: Fixed x and z offset

--- src/scene.ts
+++ src/scene.ts
@@ -1,6 +1,25 @@
 import * as BABYLON from "@babylonjs/core";
 import { GridMaterial } from "@babylonjs/materials/grid/gridMaterial";
 
+function normalizePos(vec3: Array<number>, referencePos) {
+  const SIZE = 3;
+
+  if (vec3.length < SIZE) {
+    return vec3;
+  }
+
+  console.log("BEFORE: "+ vec3);
+  for (let index = 0; index < vec3.length; ++index ) {
+    if (vec3[index] % referencePos != 0) {
+      vec3[index] += referencePos / 2;
+    }
+  }
+
+  console.log("AFTER: "+ vec3);
+
+  return vec3;
+}
+
 export default class Playground {
   private readonly boxSize = 1;
 
@@ -12,10 +31,12 @@ export default class Playground {
   private createCube(color: string, pos: BABYLON.Vector3, sizes): BABYLON.Mesh {
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
 
-    box.scaling = new BABYLON.Vector3(sizes[0],sizes[1],sizes[2]);
+    //sizes = normalizePos(sizes, this.boxSize);
+    box.scaling = new BABYLON.Vector3(sizes[0], sizes[1], sizes[2]);
 
     box.edgesWidth = 1;
     box.edgesColor = new BABYLON.Color4(1, 1, 1, 1);
+    box.setPivotPoint(new BABYLON.Vector3(this.boxSize/2,this.boxSize/2,this.boxSize/2));
     box.position = pos;
 
     box.position.y = this.groundHeight;
@@ -78,16 +99,42 @@ export default class Playground {
 
     scene.hoverCursor = "default";
 
-    let cube = this.createCube("#4A6DE5", new BABYLON.Vector3(),[1,1,1]);
-    let cube1 = this.createCube("#4912E5", new BABYLON.Vector3(3, 1, 4),[1,2,1]);
-    let cube3 = this.createCube("#43D100", new BABYLON.Vector3(2, 1, 6),[2,1,1]);
-    let cube4 = this.createCube("#97D1FF", new BABYLON.Vector3(5, 3, 0),[1,1,2]);
-    let cube5 = this.createCube("#4338DC", new BABYLON.Vector3(6, 3, 7),[1,2,2]);
-    let cube7 = this.createCube("#FFD100", new BABYLON.Vector3(-10, -4, 4),[2,1,2]);
-    let cube8 = this.createCube("#43FF00", new BABYLON.Vector3(-6, -0, 8),[2,2,1]);
-    let cube9 = this.createCube("#57D100", new BABYLON.Vector3(-4, -8, 4),[2,2,2]);
-
-    
+    let cube = this.createCube("#4A6DE5", new BABYLON.Vector3(), [1, 1, 1]);
+    let cube1 = this.createCube(
+      "#4912E5",
+      new BABYLON.Vector3(3, 1, 4),
+      [1, 2, 1]
+    );
+    let cube3 = this.createCube(
+      "#43D100",
+      new BABYLON.Vector3(2, 1, 6),
+      [2, 1, 1]
+    );
+    let cube4 = this.createCube(
+      "#97D1FF",
+      new BABYLON.Vector3(5, 3, 0),
+      [1, 1, 2]
+    );
+    let cube5 = this.createCube(
+      "#4338DC",
+      new BABYLON.Vector3(6, 3, 7),
+      [1, 2, 2]
+    );
+    let cube7 = this.createCube(
+      "#FFD100",
+      new BABYLON.Vector3(-10, -4, 4),
+      [2, 1, 2]
+    );
+    let cube8 = this.createCube(
+      "#43FF00",
+      new BABYLON.Vector3(-6, -0, 8),
+      [2, 2, 1]
+    );
+    let cube9 = this.createCube(
+      "#57D100",
+      new BABYLON.Vector3(-4, -8, 4),
+      [2, 2, 2]
+    );
 
     cube.actionManager = new BABYLON.ActionManager(scene);
 
@@ -156,9 +203,12 @@ export default class Playground {
         return;
       }
 
-      if(evt.ctrlKey) {
-        previousY = (currentY!=currentMesh.position.y)?currentY:currentMesh.position.y;
-        currentY = currentY - evt.movementY / this.decelarationDeltaY;    
+      if (evt.ctrlKey) {
+        previousY =
+          currentY != currentMesh.position.y
+            ? currentY
+            : currentMesh.position.y;
+        currentY = currentY - evt.movementY / this.decelarationDeltaY;
         if (Math.abs(currentY - previousY) > 0) {
           currentMesh.position.y = this.snap(currentY, currentMesh.scaling.y);
         }
@@ -175,7 +225,7 @@ export default class Playground {
       currentMesh.enableEdgesRendering();
 
       currentMesh.position.x = this.snap(current.x, currentMesh.scaling.x);
-      currentMesh.position.z = this.snap(current.z, currentMesh.scaling.z) + this.boxSize/2;
+      currentMesh.position.z = this.snap(current.z, currentMesh.scaling.z);
     };
 
     return scene;
