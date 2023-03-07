8e4dad33bbdaa7c9e47446c7777bef77b7b56f15
Alexandru Damian <alexdamian121@gmail.com>
2023-03-07 09:51:00 +0200

    MFG#34: Improved y axis snap

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,15 +1,23 @@
 export function snapToGrid(
-    axis: number,
-    size: number,
-    offsetAxis: number = 0,
-    snapToY: boolean = false
-  ):number {
-    let result = Math.round(axis);
+  axis: number,
+  size: number,
+  offsetAxis: number = 0
+): number {
+  let result = Math.round(axis);
 
-    if (size % 2 == 0) {
-      let sign = axis < 0 ? -1 : 1;
-      return Math.trunc(axis) + offsetAxis * sign;
-    }
-
-    return result;
+  if (size % 2 == 0) {
+    return Math.trunc(axis) + offsetAxis * sign(axis);
   }
+
+  return result;
+}
+
+export function sign(x: number): number {
+  let sign: number = Math.sign(x);
+
+  if (sign == 0) {
+    return 1;
+  }
+
+  return sign;
+}
