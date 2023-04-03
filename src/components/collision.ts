48c57f6c5b90609a5fcc9048dd0def526d839dac
Alexandru Damian <alexdamian121@gmail.com>
2023-04-03 05:34:46 +0300

    MFG#25: Fixed position offest collision check

--- src/components/collision.ts
+++ src/components/collision.ts
@@ -9,18 +9,6 @@ export class Collision {
     this._objects = objects;
   }
 
-  private getUpdatedMesh(postion: Vector3, currentObj: GameObject): Mesh {
-    let updatedMesh: Mesh = currentObj.mesh.clone("current");
-    updatedMesh.visibility = 0;
-    updatedMesh.position = postion.clone();
-    updatedMesh.computeWorldMatrix();
-
-    console.log(currentObj.mesh.position);
-    console.log(updatedMesh.position);
-
-    return updatedMesh;
-  }
-
   public isOutOfBounds(
     currentHitAxis: HitAxis,
     newDirection: number,
@@ -39,7 +27,7 @@ export class Collision {
     );
   }
 
-  private isDirectionOpposite(
+  private isDirectionChanged(
     currentObj: GameObject,
     hitAxis,
     newPosition: Vector3
@@ -68,36 +56,29 @@ export class Collision {
     newPosition: Vector3,
     currentObj: GameObject
   ): void {
-    let updatedMesh: Mesh = this.getUpdatedMesh(newPosition, currentObj);
     let hitAxisObjs: HitAxisObjs = currentObj.hitAxisObjs;
+
+    let updated: boolean = false;
 
     for (let [key, obj] of this._objects) {
       if (currentObj.mesh.id == String(key)) {
         continue;
       }
       console.log("x " + newPosition.x);
-
-      if (updatedMesh.intersectsMesh(obj.mesh)) {
+      if (
+        currentObj.mesh.intersectsMesh(obj.mesh) &&
+        !this.isDirectionChanged(currentObj, hitAxisObjs.get(obj), newPosition)
+      ) {
+        let hitAxis = hitAxisObjs.get(obj);
         currentObj.addObj(obj);
       } else {
-        if (
-          !currentObj.mesh.intersectsMesh(obj.mesh) ||
-          this.isDirectionOpposite(
-            currentObj,
-            hitAxisObjs.get(obj),
-            newPosition
-          )
-        ) {
-          hitAxisObjs.delete(obj);
-        }
+        currentObj.hitAxisObjs.delete(obj);
       }
     }
 
-    console.log(currentObj.hitAxisObjs);
-    if (currentObj.hitAxisObjs.size != 0) {
+    console.log(hitAxisObjs);
+    if (hitAxisObjs.size != 0) {
       currentObj.onCollide(newPosition);
     }
-
-    updatedMesh.dispose();
   }
 }
